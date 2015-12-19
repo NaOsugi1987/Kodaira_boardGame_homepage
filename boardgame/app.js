da7890d1eed00fbcc83e1155d9445eda6112043c
root <root@kodaira.c.rare-hub-105308.internal>
2015-12-19 06:43:27 +0000

    udate express

--- boardgame/app.js
+++ boardgame/app.js
@@ -1,12 +1,36 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
-http.listen(80);
+/**
+ * Module dependencies.
+ */
 
-app.use('/', express.static(path.resolve('public')));
-app.get('/test', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
