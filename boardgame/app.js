91e943a04883bff77ce165bed03bf1cf39bed42d
NaOsugi1987 <naosugi@r.recruit.co.jp>
2015-12-19 16:55:58 +0900

    Update app.js
    change app.js

--- boardgame/app.js
+++ boardgame/app.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var app = express();
-var path = require('path');
-var http = require('http').Server(app);
-
-http.listen(80);
-
-app.use('/', express.static(path.resolve('public')));
-app.get('/', function(req, res){
+app.listen(80);
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
