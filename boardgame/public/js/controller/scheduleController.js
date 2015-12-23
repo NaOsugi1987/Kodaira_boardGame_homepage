11d4f273aee2039cefe38c98b4476487db0c4051
takuyakubo <takuyakubo2010@gmail.com>
2015-12-23 18:56:32 +0900

    html

---
+++ boardgame/public/js/controller/scheduleController.js
@@ -0,0 +1,9 @@
+// This is a JavaScript file
+app.controller('ScheduleCtrl',function($scope){
+    $scope.eventDates= [
+        {"date":"12月23日", "hour":"1:00 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"schedules/s151223.html"},
+        {"date":"12月25日", "hour":"10:00 AM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"schedules/s151223.html"},
+        {"date":"12月27日", "hour":"6:00 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"schedules/s151223.html"},
+        {"date":"12月29日", "hour":"1:00 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"schedules/s151223.html"},
+        ];
+}) ;
