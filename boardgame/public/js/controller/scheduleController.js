5739eb0509128b934b60c270f022714d1ea22cc2
Naoya Oosugi <7842wombat3994@gmail.com>
2016-01-03 15:31:57 +0900

    write topics in home.hrml and chenge onsen toolbar and create access.html, faq.html

--- boardgame/public/js/controller/scheduleController.js
+++ boardgame/public/js/controller/scheduleController.js
@@ -1,9 +1,9 @@
 // This is a JavaScript file
 app.controller('ScheduleCtrl',function($scope){
     $scope.eventDates= [
-        {"date":"12月23日", "hour":"1:00 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"schedules/s151223.html"},
-        {"date":"12月25日", "hour":"10:00 AM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"schedules/s151223.html"},
-        {"date":"12月27日", "hour":"6:00 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"schedules/s151223.html"},
-        {"date":"12月29日", "hour":"1:00 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"schedules/s151223.html"},
+        {"date":"2016年1月23日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
+        {"date":"2016年2月20日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
+        {"date":"2016年3月5日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
+        {"date":"2016年3月5日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
         ];
 }) ;
