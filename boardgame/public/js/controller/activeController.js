e61407b2a5e147d00fdef005534a086f59bb35be
Naoya Oosugi <7842wombat3994@gmail.com>
2016-01-16 15:17:49 +0900

    コンテンツ強化

--- boardgame/public/js/controller/activeController.js
+++ boardgame/public/js/controller/activeController.js
@@ -1,10 +1,10 @@
 // This is a JavaScript file
 app.controller('ActivityCtrl',function($scope){
     $scope.contents= [
-        {"date":"15/12/23", "desc":"説明1", "htmls":"activities/a151223.html"},
-        {"date":"15/12/24", "desc":"説明2", "htmls":"activities/a151223.html"},
-        {"date":"15/12/25", "desc":"説明3", "htmls":"activities/a151223.html"},
-        {"date":"15/12/26", "desc":"説明4", "htmls":"activities/a151223.html"},
-        {"date":"15/12/27", "desc":"説明5", "htmls":"activities/a151223.html"},
+    	{"date":"2011年度", "desc":"会の活動が始まりました", "htmls":"activities/2011.html"},
+        {"date":"2012年度", "desc":"ゲームもスタッフも順調に増えました", "htmls":"activities/2012.html"},
+        {"date":"2013年度", "desc":"子ども夢基金の助成を受け始めました", "htmls":"activities/2013.html"},
+        {"date":"2014年度", "desc":"小平市教育委員会の後援を受け始めました", "htmls":"activities/2014.html"},
+        {"date":"2015年度", "desc":"公民館地下の和室いっぱいに人が訪れるようになりました", "htmls":"activities/2015.html"},
         ];
 }) ;
