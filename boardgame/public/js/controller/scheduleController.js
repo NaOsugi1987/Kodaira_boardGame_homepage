63b88a9c245ff44ea1cd6a0b3f32f9421a0416d2
Naoya Oosugi <7842wombat3994@gmail.com>
2016-01-31 17:49:51 +0900

    スケジュール更新

--- boardgame/public/js/controller/scheduleController.js
+++ boardgame/public/js/controller/scheduleController.js
@@ -4,6 +4,11 @@ app.controller('ScheduleCtrl',function($scope){
         {"date":"2016年1月23日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
         {"date":"2016年2月20日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
         {"date":"2016年3月5日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-        {"date":"2016年3月5日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-        ];
+        {"date":"2016年4月23日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
+        {"date":"2016年5月28日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
+		{"date":"2016年6月25日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
+		{"date":"2016年7月23日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
+		{"date":"2016年8月20日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
+		{"date":"2016年9月10日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
+];
 }) ;
