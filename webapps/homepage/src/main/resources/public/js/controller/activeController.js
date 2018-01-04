9545fe70e56a25c3a99ff839973f7c6ca44321bc
akaisora7 <want.my.happiness@gmail.com>
2018-01-04 17:17:29 +0900

    前のホームページを移植。 前のページはindex.htmlの単一ページのみで、表示の切り替えは静的リソース読み込みで行っていたのでindex.htmlのみspring mvc管理にして、その他は全て静的リソースにした。 ここからスケジュールデータの受け渡し方法や、登録UIの実装に取り掛かっていく。

---
+++ webapps/homepage/src/main/resources/public/js/controller/activeController.js
@@ -0,0 +1,10 @@
+// This is a JavaScript file
+app.controller('ActivityCtrl',function($scope){
+    $scope.contents= [
+    	{"date":"2011年度", "desc":"会の活動が始まりました", "htmls":"activities/2011.html"},
+        {"date":"2012年度", "desc":"ゲームもスタッフも順調に増えました", "htmls":"activities/2012.html"},
+        {"date":"2013年度", "desc":"子ども夢基金の助成を受け始めました", "htmls":"activities/2013.html"},
+        {"date":"2014年度", "desc":"小平市教育委員会の後援を受け始めました", "htmls":"activities/2014.html"},
+        {"date":"2015年度", "desc":"公民館地下の和室いっぱいに人が訪れるようになりました", "htmls":"activities/2015.html"},
+        ];
+}) ;
