f739d3dc6cad9239ff5dad7a035a7c8b55b66b3b
soranakk <want.my.happiness@gmail.com>
2018-02-12 23:26:24 +0900

    #26 Googleカレンダーから予定を取ってくる
    * スケジュール情報を更新するAPIのレスポンスは、更新後のスケジュール一覧のjsonにしました。

--- webapps/homepage/src/main/kotlin/jp/kodaira/boardgame/homepage/controller/ScheduleController.kt
+++ webapps/homepage/src/main/kotlin/jp/kodaira/boardgame/homepage/controller/ScheduleController.kt
@@ -1,19 +1,18 @@
 package jp.kodaira.boardgame.homepage.controller
 
 import jp.kodaira.boardgame.homepage.domain.GoogleCalendarModel
+import jp.kodaira.boardgame.homepage.domain.ScheduleModel
 import jp.kodaira.boardgame.homepage.properties.GoogleCalendarProperties
 import jp.kodaira.boardgame.homepage.service.ScheduleService
 import org.slf4j.LoggerFactory
 import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.stereotype.Controller
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestOperations
 import org.springframework.web.util.UriComponentsBuilder
 
-
-@Controller
+@RestController
 class ScheduleController(private val scheduleService: ScheduleService,
                          private val googleCalendarProperties: GoogleCalendarProperties,
                          private val restTemplateBuilder: RestTemplateBuilder) {
@@ -23,7 +22,8 @@ class ScheduleController(private val scheduleService: ScheduleService,
 
     @Transactional
     @GetMapping("/schedule/update")
-    fun scheduleUpdate(model: Model): String {
+    fun scheduleUpdate(): List<ScheduleModel> {
+        logger.info("scheduleUpdate")
         // urlを生成
         val urlBuilder = UriComponentsBuilder
                 .fromHttpUrl(googleCalendarProperties.calendarUrl)
@@ -34,8 +34,7 @@ class ScheduleController(private val scheduleService: ScheduleService,
         val googleCalendarModel = restOperations.getForObject(urlBuilder.toUri(), GoogleCalendarModel::class.java)
 
         // スケジュール情報に変換してDBに保存する
-        val scheduleList = scheduleService.upsert(googleCalendarModel)
-
-        return "index"
+        scheduleService.upsert(googleCalendarModel)
+        return scheduleService.selectAll()
     }
 }
