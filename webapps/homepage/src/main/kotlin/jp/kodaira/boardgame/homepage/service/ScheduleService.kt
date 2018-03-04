7b5bb329f55eba9f4befe130fb3b6e7f7a62fa37
soranakk <want.my.happiness@gmail.com>
2018-03-04 11:36:11 +0900

    #28 内部DBのwebコンソールアクセスを提供する
    スケジュール取得コントローラーの処理をサービス側に移動させました。
    GoogleCalendarから情報を取得する部分をクラスに分離しました。

--- webapps/homepage/src/main/kotlin/jp/kodaira/boardgame/homepage/service/ScheduleService.kt
+++ webapps/homepage/src/main/kotlin/jp/kodaira/boardgame/homepage/service/ScheduleService.kt
@@ -1,28 +1,44 @@
 package jp.kodaira.boardgame.homepage.service
 
-import jp.kodaira.boardgame.homepage.domain.GoogleCalendarModel
+import jp.kodaira.boardgame.homepage.client.GoogleCalendarClient
 import jp.kodaira.boardgame.homepage.domain.ScheduleModel
 import jp.kodaira.boardgame.homepage.mapper.ScheduleMapper
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
-class ScheduleService(private val scheduleMapper: ScheduleMapper) {
+class ScheduleService(
+        private val googleCalendarClient: GoogleCalendarClient,
+        private val scheduleMapper: ScheduleMapper) {
+    companion object {
+        private val logger = LoggerFactory.getLogger(ScheduleService::class.java)
+    }
 
     /**
-     * GoogleCalendarModelをScheduleModelに変換してDBにupsertする
+     * スケジュール情報を更新して更新後のスケジュール情報を返す
      */
-    fun upsert(googleCalendarModel: GoogleCalendarModel?) =
-            googleCalendarModel?.items?.map {
-                val newScheduleModel = ScheduleModel(
-                        startTime = it.start.dateTime,
-                        endTime = it.end.dateTime,
-                        summary = it.summary,
-                        location = it.location)
-                scheduleMapper.upsert(newScheduleModel)
-                newScheduleModel
-            } ?: listOf()
+    fun update(): List<ScheduleModel> {
+        // Google Calendarから予定を取得する
+        val googleCalendarModel = googleCalendarClient.getGoogleCalendar()
+        if (googleCalendarModel == null) {
+            logger.warn("googleCalendarModel is null")
+            return selectAll()
+        }
+        // スケジュール情報に変換してDBに保存する
+        googleCalendarModel.items.map {
+            val newScheduleModel = ScheduleModel(
+                    startTime = it.start.dateTime,
+                    endTime = it.end.dateTime,
+                    summary = it.summary,
+                    location = it.location)
+            scheduleMapper.upsert(newScheduleModel)
+            newScheduleModel
+        }
+        // 更新したスケジュール情報を返す
+        return selectAll()
+    }
 
     /**
      * スケジュール情報をすべて取得する
