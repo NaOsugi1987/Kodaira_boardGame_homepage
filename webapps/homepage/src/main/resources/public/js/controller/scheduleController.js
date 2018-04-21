4864ac9a9e3fd7be04453c466926170036996671
大杉 直也 <naosugi@r.recruit.co.jp>
2018-04-21 22:36:25 +0900

    スケジュールをgoogle API の形式で受けられるように修正

--- webapps/homepage/src/main/resources/public/js/controller/scheduleController.js
+++ webapps/homepage/src/main/resources/public/js/controller/scheduleController.js
@@ -1,14 +1,34 @@
 // This is a JavaScript file
 app.controller('ScheduleCtrl',function($scope){
-    $scope.eventDates= [
-        {"date":"2016年1月23日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-        {"date":"2016年2月20日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-        {"date":"2016年3月5日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-        {"date":"2016年4月23日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-        {"date":"2016年5月28日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-		{"date":"2016年6月25日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-		{"date":"2016年7月23日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-		{"date":"2016年8月20日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-		{"date":"2016年9月10日", "hour":"1:30 PM","name":"小平ボードゲーム", "place":"小平市中央公民館","htmls":"access.html"},
-];
+
+    eventDates = [
+    {"startTime":"2018-01-27T13:30:00+09:00","endTime":"2018-01-27T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-02-17T13:30:00+09:00","endTime":"2018-02-17T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-03-03T13:30:00+09:00","endTime":"2018-03-03T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-04-21T13:30:00+09:00","endTime":"2018-04-21T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-05-19T13:30:00+09:00","endTime":"2018-05-19T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-06-16T13:30:00+09:00","endTime":"2018-06-16T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-07-21T13:30:00+09:00","endTime":"2018-07-21T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-08-18T13:30:00+09:00","endTime":"2018-08-18T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-09-08T13:30:00+09:00","endTime":"2018-09-08T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-10-20T13:30:00+09:00","endTime":"2018-10-20T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-11-17T13:30:00+09:00","endTime":"2018-11-17T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2018-12-15T13:30:00+09:00","endTime":"2018-12-15T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2019-01-19T13:30:00+09:00","endTime":"2019-01-19T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2019-02-16T13:30:00+09:00","endTime":"2019-02-16T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"},
+    {"startTime":"2019-03-02T13:30:00+09:00","endTime":"2019-03-02T15:30:00+09:00","summary":"みんなで遊ぼうドイツゲーム","location":"小平市中央公民館"}
+    ];
+
+    for (i = 0; i < eventDates.length; i++) {
+        var sch = eventDates[i];
+        var startTime = new Date(sch['startTime']);
+        var endTime = new Date(sch['endTime']);
+        sch['yyyymmdd'] = (startTime.getYear()+1900) + "/" + (startTime.getMonth()+1) + "/" + startTime.getDate();
+        sch['hhmm'] = startTime.getHours() + ":" + startTime.getMinutes() + "~" + endTime.getHours() + ":" + endTime.getMinutes();
+        eventDates[i] = sch;
+    }
+
+    $scope.eventDates = eventDates;
+
+
 }) ;
